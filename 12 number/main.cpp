9326c5c4736744a8eabf145ba4a2228c69e2e3f4
MD Jahid Hasan Hridoy <38782135+mdjhhbd@users.noreply.github.com>
2023-09-04 19:32:36 +0600

    Add files via upload

---
+++ 12 number/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*int i;
+    cin >> i;
+    if(i<50){
+        cout << "You are not allowed" << endl;
+    }
+    else if(i<=50){
+        cout << "You are allowed" << endl;
+    }
+    else{
+        cout << "The Fild is empty" << endl;
+    }*/
+    int i, j, x;
+    cin >> x;
+    for (i=x; i<=0; i--) {
+        cout << i << "\n";
+    }
+    return 0;
+}
